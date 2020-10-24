f1ae77728363275a8e6a6eee8aee896ae1ae12ba
github username <60378440+vmar13@users.noreply.github.com>
2020-10-24 11:33:59 -0400

    updated binarySearch

--- binarySearch.js
+++ binarySearch.js
@@ -1,21 +1,55 @@
+//only use on SORTED array
+
+//Write a function called binarySearch which accepts a sorted array and a value and 
+//returns the index at which the value exists. Otherwise, return -1.
+
 const binarySearch = (arr, val) => {
-    let left = 0;
-    let right= arr.length - 1;
-    let middle = Math.floor((left + right) / 2);
-    console.log(left, right, middle)
+    let left = 0
+    let right= arr.length - 1
+    let middle = Math.floor((left + right) / 2)
+    // console.log(left, right, middle)
+
+    //SHORTER CODE
+    while(arr[middle] !== val && left <= right){
+        if(val < arr[middle]) right = middle - 1
+        else left = middle + 1
+        middle = Math.floor((left + right) / 2)
+      }
+        return arr[middle] === val ? middle : -1
+    }
   
-  //   while(left < right){
-  
-  //     if(middle === val){
-  //       return arr.indexOf[middle]
-  //     } else if (middle < val){
-  //       left++
-  //     } else if (middle > val) {
-  //       right--
+    //LONGER CODE
+  //   while(arr[middle] !== val && left <= right){
+  //     if(val < arr[middle]){
+  //       right = middle - 1
   //     } else {
-  //       return -1
+  //       left = middle + 1
   //     }
+  //     middle = Math.floor((left + right) / 2)
   //   }
-  }
+  //     if(arr[middle] === val){
+  //       return middle
+  //     } 
+  //     return -1
+  // }
+
+  console.log(binarySearch([1,2,3,4,5,8,10,13,14,17,20], 17))
+
+
+  //WRONG CODE - first attempt 
   
-  binarySearch([1,2,3,4,5,8,10,13,14,17,20], 10)
+    // while(left < right){
+  
+    //   if(middle === val){
+    //     return arr.indexOf[middle]
+    //   } else if (middle < val){
+    //     left++
+    //   } else if (middle > val) {
+    //     right--
+    //   } else {
+    //     return -1
+    //   }
+    // }
+  // }
+  
+  
