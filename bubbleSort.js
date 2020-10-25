551275d6e5d4e8ff0914de30d1006d32362a7ab2
github username <60378440+vmar13@users.noreply.github.com>
2020-10-25 18:45:12 -0400

    updated bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -5,7 +5,9 @@
 // Return the sorted array.
 
 const bubbleSort = (arr) => {
+    let noSwaps;
     for(let i = arr.length; i > 0; i--){
+        noSwaps = true;
         for(let j = 0; j < i - 1; j++){
             console.log(arr, arr[j], arr[j+1])
             if(arr[j] > arr[j+1]){
@@ -13,8 +15,10 @@ const bubbleSort = (arr) => {
                 let temp = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = temp 
+                noSwaps = false;
             }
         }
+        if(noSwaps) break;
     }
     return arr
 }
