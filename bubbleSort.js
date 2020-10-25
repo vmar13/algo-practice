fffae391e2d7f01be4575221d97125f84006837a
github username <60378440+vmar13@users.noreply.github.com>
2020-10-25 17:54:13 -0400

    added bubbleSort

---
+++ bubbleSort.js
@@ -0,0 +1,22 @@
+//Start looping with a variable called i from the end of the array
+// towards the beginning. Start an inner loop with a variable
+// called j from the beginning until i - 1.
+// If arr[j] is greater than arr[j + 1], swap those two values
+// Return the sorted array.
+
+const bubbleSort = (arr) => {
+    for(let i = arr.length; i > 0; i--){
+        for(let j = 0; j < i - 1; j++){
+            console.log(arr, arr[j], arr[j+1])
+            if(arr[j] > arr[j+1]){
+                //SWAP
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp 
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort([37, 45, 58, 9]))
