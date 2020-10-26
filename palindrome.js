9c4af7551cd33bd6567274164aace5d40da21439
github username <60378440+vmar13@users.noreply.github.com>
2020-10-26 12:19:08 -0400

    refactored palin5

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,15 @@
 const palin5 = str => {
-    str = str.toLowerCase().replace(/[\W]/g, '')
+    str = str.toLowerCase().replace(/[\W_]/g, '')
     let reversedStr = str.split('').reverse().join('')
 
-    if(str === reversedStr){
-        return true
-    } else {
-        return false
-    }
+    if(str === reversedStr) return true
+    else return false 
+    // if(str === reversedStr){
+    //     return true
+    // } else {
+    //     return false
+    // }
 }
 
-palin5('Race car')
+console.log(palin5('oce3!!nTT_'))
+
