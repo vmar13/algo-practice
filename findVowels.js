5922a01892738c27549412fe189b49519f1a1e57
github username <60378440+vmar13@users.noreply.github.com>
2020-10-25 21:38:29 -0400

    redid fizzbuzz and findVowels

--- findVowels.js
+++ findVowels.js
@@ -1,6 +1,19 @@
-const findVowelsCount1 = str => {
-    const foundVs = str.match(/[aeiou]/gi)
-    return foundVs ? foundVs.length : 0
+// Write a function that accepts a string as a parameter and counts the 
+// number of vowels within the string.
+
+const countVowels = str => {
+    let count = str.match(/[aeiou]/gi)
+    return count ? count.length : 0
 }
 
-findVowelsCount1('ocean')
+console.log(countVowels('plk'))
+
+
+
+
+// const findVowelsCount1 = str => {
+//     const foundVs = str.match(/[aeiou]/gi)
+//     return foundVs ? foundVs.length : 0
+// }
+
+// findVowelsCount1('ocean')
--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,15 +1,39 @@
+// Write a program that prints the numbers from 1 to 100. But for multiples of three print,
+//  “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+//  For numbers which are multiples of both three and five print “FizzBuzz”.
+
 const fizzBuzz = n => {
     for(let i = 1; i <= n; i++){
-       if(i % 3 === 0 && i % 5 === 0){
-        console.log('fizzbuzz')
-    } else if (i % 3 === 0){
-        console.log('fizz')
-    } else if (i % 5 === 0){
-        console.log('buzz')
-    } else {
-        console.log(i)
-    } 
-   }
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log('FizzBuzz') 
+        } else if (i % 3 === 0){
+            console.log('Fizz')
+        } else if (i % 5 === 0){
+            console.log('Buzz')
+        } else {
+            console.log(i) 
+        }
+    }
 }
 
-fizzBuzz('50')
+console.log(fizzBuzz(100))
+
+
+
+
+
+// const fizzBuzz = n => {
+//     for(let i = 1; i <= n; i++){
+//        if(i % 3 === 0 && i % 5 === 0){
+//         console.log('fizzbuzz')
+//     } else if (i % 3 === 0){
+//         console.log('fizz')
+//     } else if (i % 5 === 0){
+//         console.log('buzz')
+//     } else {
+//         console.log(i)
+//     } 
+//    }
+// }
+
+// fizzBuzz('50')
