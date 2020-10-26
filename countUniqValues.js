aee4b9aeb2ecbd59e0e92210c7d16390627ebc0e
github username <60378440+vmar13@users.noreply.github.com>
2020-10-26 12:45:59 -0400

    refactored countUniqVals

--- countUniqValues.js
+++ countUniqValues.js
@@ -1,12 +1,34 @@
-function countUniqueValues(sortedArr){
+//Write a function called countUniqValues that takes in a sorted array
+//and counts the number of unique integers. Return the count.
+
+const countUniqVals = sortedArr => {
     let i = 0
-    for(let j=1; j < sortedArr.length; j++){
-        if(sortedArr[i] !== sortedArr[j]){
-        i++
+
+    for(let j = 1; j < sortedArr.length; j++){
+        if(sortedArr[i] !== sortedArr[j]) i++
         sortedArr[i] = sortedArr[j]
-    } 
     }
     return i + 1
-  }
+}
   
-  countUniqueValues([1,2,3,4,4,7,7,12,12,13])
+  console.log(countUniqVals([1,2,3,4,4,7,7,12,12,13,32,45,78]))
+
+
+
+
+
+
+
+
+
+
+//   function countUniqueValues(sortedArr){
+//     let i = 0
+//     for(let j=1; j < sortedArr.length; j++){
+//         if(sortedArr[i] !== sortedArr[j]){
+//         i++
+//         sortedArr[i] = sortedArr[j]
+//     } 
+//     }
+//     return i + 1
+//   }
