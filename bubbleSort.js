f96b97d4a272fba9c7a1c716026634defad24f54
github username <60378440+vmar13@users.noreply.github.com>
2020-10-29 17:29:13 -0400

    added swap options to bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -24,3 +24,17 @@ const bubbleSort = (arr) => {
 }
 
 console.log(bubbleSort([37, 45, 58, 9]))
+
+//----SWAP OPTIONS---//
+
+//ES5
+function swap(arr, idx1, idx2) {
+    let temp = arr[idx1]
+    arr[idx1] = arr[idx2]
+    arr[idx2] = temp 
+}
+
+//ES2015
+const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+}
