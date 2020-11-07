510610e61652d0e483a146ee41766c51ff6b79b8
github username <60378440+vmar13@users.noreply.github.com>
2020-11-07 18:40:17 -0500

    added removeDups second solution

--- removeDups.js
+++ removeDups.js
@@ -1,4 +1,15 @@
+// function removeDuplicates(nums) {
+//     nums = nums.filter((int, index) => nums.indexOf(int) === index)
+//     return nums.length
+// }
+
 function removeDuplicates(nums) {
-    nums = nums.filter((int, index) => nums.indexOf(int) === index)
-    return nums.length
+    let i = 0
+    for(let j = 1; j < nums.length; j++){
+        if(nums[j] !== nums[i]){
+            i++
+            nums[i] = nums[j]
+        }
+    }
+    return i + 1
 }
