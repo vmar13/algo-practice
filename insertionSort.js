63c23b128aab59589ab5c303fe8df264edaba3ef
github username <60378440+vmar13@users.noreply.github.com>
2020-11-10 17:56:52 -0500

    final for  insertionSort

--- insertionSort.js
+++ insertionSort.js
@@ -12,15 +12,18 @@ const insertionSort = arr => {
     for(let i = 1; i < arr.length; i++){
         let currentVal = arr[i]
         for(let j = i - 1; j >= 0 && arr[j] > currentVal; j--){
-            
+            arr[j + 1] = arr[j]
         }
-        // if(arr[i] < arr[i - 1]){
-        //     let temp = arr[i - 1]
-        //     arr[i - 1] = arr[i]
-        //     arr[i] = temp
-        // }
+        arr[j + 1] = currentVal
+      
     }
     return arr 
 }
 
 console.log(insertionSort(arr))
+
+  // if(arr[i] < arr[i - 1]){
+        //     let temp = arr[i - 1]
+        //     arr[i - 1] = arr[i]
+        //     arr[i] = temp
+        // }
