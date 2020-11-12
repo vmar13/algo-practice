1adffabc2521fd3fea468a0309bb5b39c36973da
github username <60378440+vmar13@users.noreply.github.com>
2020-11-11 20:46:43 -0500

    organized beginning logic for mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -37,4 +37,13 @@ const getHalf = arr => {
     
 }
 
-console.log(getHalf(arr))
+// console.log(getHalf(arr))
+
+const mergeSort = arr => {
+    if(arr.length <= 1) return arr
+    let mid = Math.floor(arr.length/2)
+    let left = mergeSort(arr.slice(0,mid))
+    let right = mergeSort(arr.slice(mid))
+}
+
+console.log(mergeSort(arr))
