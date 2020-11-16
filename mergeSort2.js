75cd93853e3dc39aa7eeaa7070535698c6b05c7a
github username <60378440+vmar13@users.noreply.github.com>
2020-11-16 16:16:55 -0500

    practicing merging arrays for mergeSort2

---
+++ mergeSort2.js
@@ -0,0 +1,22 @@
+//Split arr in half
+//Split that half in half... keep splitting until you have single-element arrays.
+//Merge every 2 single-sorted arrays into 1 (comparing 2 values)
+//Merge every 2 double-digit arrays into 1 (comparing 4 values)
+//Merge every 2 four-digit arrays into 1 (comparing 8 values)
+//Continue until you reach the final merge
+
+const merge = (arr1, arr2) => {
+    let results = []
+    let i = 0
+    let j = 0
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i])
+            i++
+        } else {
+            results.push(arr2[j])
+            j++
+        }
+    }
+    return results
+}
