5fcb796e6a2c70d28bbf5a30ed36e8eda7911200
github username <60378440+vmar13@users.noreply.github.com>
2020-11-15 19:33:06 -0500

    added pivot to quickSort

--- quickSort.js
+++ quickSort.js
@@ -1,9 +1,22 @@
-let pivot = arr[start]
-let swapIdx = start 
-
-for(let i = start + 1; i < arr.length; i++){
-    if(pivot > arr[i]){
-        swapIdx++
-        swapIdx(arr, swapIdx, i)
+const pivot = (arr, start=0, end=arr.length+1) => {
+    const swap =(array, i, j) => {
+        let temp = array[i]
+        array[i] = array[j]
+        array[j] = temp 
     }
+
+
+    let pivot = arr[start]
+    let swapIdx = start 
+
+    for(let i = start + 1; i < arr.length; i++){
+        if(pivot > arr[i]){
+            swapIdx++
+            swapIdx(arr, swapIdx, i)
+        }
+    }
+    swap(arr, start, swapIdx)
+    return swapIdx
 }
+
+console.log(pivot([9,4,8,2,1]))
