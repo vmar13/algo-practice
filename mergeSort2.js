925131e51d10dd47d94d14380c0086a03c7dc1b6
github username <60378440+vmar13@users.noreply.github.com>
2020-11-16 18:12:16 -0500

    found both halves in arr for mergeSort2

--- mergeSort2.js
+++ mergeSort2.js
@@ -1,3 +1,31 @@
+//-----MERGE-----//
+
+// const merge = (arr1, arr2) => {
+//     let results = []
+//     let i = 0
+//     let j = 0
+//     while(i < arr1.length && j < arr2.length){
+//         if(arr2[j] > arr1[i]){
+//             results.push(arr1[i])
+//             i++
+//         } else {
+//             results.push(arr2[j])
+//             j++
+//         }
+//     }
+//     while(i < arr1.length){
+//         results.push(arr1[i])
+//         i++
+//     }
+//     while(j < arr2.length){
+//         results.push(arr2[j])
+//         j++
+//     }
+//     return results
+// }
+
+// console.log(merge([2,3,7,10], [5,8,60]))
+
 //Split arr in half
 //Split that half in half... keep splitting until you have single-element arrays.
 //Merge every 2 single-sorted arrays into 1 (comparing 2 values)
@@ -5,28 +33,23 @@
 //Merge every 2 four-digit arrays into 1 (comparing 8 values)
 //Continue until you reach the final merge
 
-const merge = (arr1, arr2) => {
-    let results = []
-    let i = 0
-    let j = 0
-    while(i < arr1.length && j < arr2.length){
-        if(arr2[j] > arr1[i]){
-            results.push(arr1[i])
-            i++
-        } else {
-            results.push(arr2[j])
-            j++
-        }
-    }
-    while(i < arr1.length){
-        results.push(arr1[i])
-        i++
-    }
-    while(j < arr2.length){
-        results.push(arr2[j])
-        j++
-    }
-    return results
+let arr = [2, 3, 5, 7, 8, 10, 60]
+
+const mergeSort = arr => {
+    if(arr.length <= 1) return arr 
+
+    let half = Math.floor(arr.length / 2)
+    let firstHalf = arr.slice(0,half)
+    let secHalf = arr.slice(half)
+    return firstHalf
 }
 
-console.log(merge([2,3,7,10], [5,8,60]))
+// const getHalf = arr => {
+//     let half =  Math.floor(arr.length / 2)
+
+//     return half
+// }
+
+console.log(mergeSort(arr))
+//-----SORT-----//
+
