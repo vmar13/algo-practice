22d32fc63e63865372f7f7b0231673081a3ce5e0
github username <60378440+vmar13@users.noreply.github.com>
2020-11-18 17:57:48 -0500

    done  maxChar

--- maxChar.js
+++ maxChar.js
@@ -6,4 +6,12 @@ const maxChar = str => {
     for(let char of str){
         strObj[char] = strObj[char] + 1 || 1
     }
+
+    for(let char in strObj){
+        if(strObj[char] > maxValue){
+            maxChar = char 
+            maxValue = strObj[char]
+        }
+    }
+    return maxChar
 }
