f504aa74214b8a516ad8a336af2543389f3b5209
github username <60378440+vmar13@users.noreply.github.com>
2020-11-19 18:02:44 -0500

    fixed error in prev solution for singleNum

--- singleNum.js
+++ singleNum.js
@@ -1,17 +1,23 @@
 //Given a non-empty array of integers nums, every element appears twice except for one.
 //Find that single one.
 
-let nums = [2,2,1]
+let numbers = [2,2,1,1,4,5,5]
 
 const singleNum = nums => {
     let counterObj = {}
     
     for(let num of nums){
         counterObj[num] = counterObj[num] + 1 || 1
-        if(counterObj[num] === 1){
-            return counterObj[num]
-        } 
+        // if(counterObj[num] === 1){
+        //     return counterObj[num]
+        // } 
     }   
+    
+    for(let num in counterObj){
+        if(counterObj[num] === 1){
+            return num
+        }
+    }
 }
 
-console.log(singleNum(nums))
+console.log(singleNum(numbers))
