faf7d450d699f165786953b73c5c1994151a380b
github username <60378440+vmar13@users.noreply.github.com>
2020-11-20 14:58:33 -0500

    solved rotateArr without creating new arr

--- rotateArr.js
+++ rotateArr.js
@@ -2,6 +2,7 @@
 
 let nums = [1,2,3,4,5,6,7]
 
+//----SOLUTION 1---//
 // const rotate = (nums, k) => {
 //     let rotated = nums.splice(-k)
 
@@ -12,11 +13,32 @@ let nums = [1,2,3,4,5,6,7]
 // }
 
 
-const rotate = (nums, k) => {
-    let rotated = nums.splice(-k)
+//----SOLUTION 2---//
+// const rotate = (nums, k) => {
+//     let rotated = nums.splice(-k)
 
-    rotated.push(...nums)
-    return rotated
+//     rotated.push(...nums)
+//     return rotated
+// }
+
+//----SOLUTION 3---//
+
+const rotate = (nums, k) => {
+    k = k % nums.length
+
+    reverse(nums,0,nums.length - 1)
+    reverse(nums,0,k - 1)
+    reverse(nums,k,nums.length - 1)
 }
 
-console.log(rotate(nums, 2))
+const reverse = (nums, start, end) => {
+    while(start < end){
+        let temp = nums[start]
+        nums[start] = nums[end]
+        nums[end] = temp
+        start++
+        end--
+    }
+}
+
+console.log(rotate(nums, 3))
