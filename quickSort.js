c574a380a644962413c636e4df994f722f44c38d
github username <60378440+vmar13@users.noreply.github.com>
2020-11-21 20:31:20 -0500

    recursion for quickSort

--- quickSort.js
+++ quickSort.js
@@ -8,14 +8,27 @@
 //element at the pivot index.
 
 const pivot = (arr, start=0, end=arr.length+1) => {
+    const swap = (arr, i , j) => {
+        let temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp 
+    }
+
     let pivot = arr[start]
     let swapIdx = start
 
     for(let i = start + 1; i < arr.length; i++){
         if(pivot > arr[i]){
             swapIdx++
+            swap(arr,swapIdx,i)
         }
     }
 }
+
+    const quickSort = (arr, left=0, right=arr.length - 1) => {
+        let pivotIndex = pivot(arr, left, right)
+
+        quickSort(arr,left,pivot)
+    }
 
 console.log(pivot([9,4,8,2,1]))
