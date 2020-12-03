ce4303e456a63c61100debf32e20a62acf7917e4
github username <60378440+vmar13@users.noreply.github.com>
2020-12-02 20:10:42 -0500

    added blockStart and blockEnd to jumpSearch

--- jumpSearch.js
+++ jumpSearch.js
@@ -1,9 +1,17 @@
 const jumpSearch = (sortedArray, seekElement, comparatorCallback) => {
-    const comparator = new Comparator(comparatorCallback);
-    const arraySize = sortedArray.length;
+    const comparator = new Comparator(comparatorCallback)
+    const arraySize = sortedArray.length
   
     if (!arraySize) {
-      return -1;
+      return -1
+    }
+  
+    const jumpSize = Math.floor(Math.sqrt(arraySize))
+  
+    let blockStart = 0
+    let blockEnd = jumpSize
+    while (comparator.greaterThan(seekElement, sortedArray[Math.min(blockEnd, arraySize) - 1])) {
+      
     }
   
     
