1b2feef3a52ef564e9d24dd73eada28daa8f14da
github username <60378440+vmar13@users.noreply.github.com>
2020-12-02 20:05:44 -0500

    added maxSubArray

---
+++ maxSubArr.js
@@ -0,0 +1,12 @@
+const bfMaximumSubarray = inputArray => {
+    let maxSubarrayStartIndex = 0
+    let maxSubarrayLength = 0
+    let maxSubarraySum = null
+  
+    for (let startIndex = 0; startIndex < inputArray.length; startIndex += 1) {
+      let subarraySum = 0
+      
+    }
+  
+    return inputArray.slice(maxSubarrayStartIndex, maxSubarrayStartIndex + maxSubarrayLength)
+  }
