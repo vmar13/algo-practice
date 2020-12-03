6e3f8a14999128d2280d6fb230e5c2ece240d407
github username <60378440+vmar13@users.noreply.github.com>
2020-12-03 16:26:48 -0500

    successfully iterated over each objEle within array

---
+++ sapphireDigi.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', e => {
+
+    const getMostRecentDeploys = () => {
+        fetch('https://gist.githubusercontent.com/gretchenziegler/853c4f709d45176aa44c8e5aee864cac/raw/010a4c44455ffc93b8039935cfc4e0dff41ae502/hcsc.json')
+        .then(res => res.json())
+        .then(data => {
+            data.client_environments.forEach(objEle => {
+                console.log(objEle.most_recent_deploys)
+            })      
+        })  
+    }
+
+    
+getMostRecentDeploys()
+
+})
