f991530411f74760de05ae4839e9cce1ac0ac886
github username <60378440+vmar13@users.noreply.github.com>
2020-12-02 20:08:42 -0500

    setting up jumpSearch

---
+++ jumpSearch.js
@@ -0,0 +1,10 @@
+const jumpSearch = (sortedArray, seekElement, comparatorCallback) => {
+    const comparator = new Comparator(comparatorCallback);
+    const arraySize = sortedArray.length;
+  
+    if (!arraySize) {
+      return -1;
+    }
+  
+    
+  }
