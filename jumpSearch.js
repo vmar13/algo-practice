961d09a618920b16cf7331966e779aa814d239dd
github username <60378440+vmar13@users.noreply.github.com>
2020-12-02 20:11:43 -0500

    finished jumpSearch

--- jumpSearch.js
+++ jumpSearch.js
@@ -5,14 +5,28 @@ const jumpSearch = (sortedArray, seekElement, comparatorCallback) => {
     if (!arraySize) {
       return -1
     }
+    const jumpSize = Math.floor(Math.sqrt(arraySize));
   
-    const jumpSize = Math.floor(Math.sqrt(arraySize))
-  
-    let blockStart = 0
-    let blockEnd = jumpSize
+    let blockStart = 0;
+    let blockEnd = jumpSize;
     while (comparator.greaterThan(seekElement, sortedArray[Math.min(blockEnd, arraySize) - 1])) {
-      
+      blockStart = blockEnd
+      blockEnd += jumpSize
+  
+      if (blockStart > arraySize) {
+        return -1
+      }
     }
   
+    let currentIndex = blockStart
+    while (currentIndex < Math.min(blockEnd, arraySize)) {
+      if (comparator.equal(sortedArray[currentIndex], seekElement)) {
+        return currentIndex
+      }
+  
+      currentIndex += 1
+    }
+  
+    return -1
     
   }
