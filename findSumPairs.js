f31c391019061f7b127b791d47047d911f50c190
github username <60378440+vmar13@users.noreply.github.com>
2020-12-15 21:56:57 -0500

    setting up findSumPairs

---
+++ findSumPairs.js
@@ -0,0 +1,14 @@
+let arr = [1,5,6,1,0,1]
+
+const findSumPairs = (arr, value) => {
+  let sumsLookup = {}
+  let output = []
+  
+  for(let i = 0; i < arr.length; i++) {
+    let targetVal = value - arr[i]
+    
+  }
+  
+  return output
+}
+console.log(findSumPairs(arr, 6))
