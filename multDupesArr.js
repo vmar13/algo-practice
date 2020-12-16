e39f08d7c1c449c4ef791987517a9665e1c0f3b1
github username <60378440+vmar13@users.noreply.github.com>
2020-12-15 21:51:47 -0500

    setting up multDupesArr

---
+++ multDupesArr.js
@@ -0,0 +1,14 @@
+const arr = [1,1,2,3,4,5,6,7,8,6,6,7,7,7,10,10]
+
+const multDupesArr = arr => {
+  let observed = {}
+  let dupesArr = []
+  
+  for(let i = 0; i < arr.length; i++) {
+ 
+  }
+  
+  return dupesArr
+}
+
+console.log(multDupesArr(arr))
