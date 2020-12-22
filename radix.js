c5df232005c617d56e87451457bd4b36d4919bda
github username <60378440+vmar13@users.noreply.github.com>
2020-12-22 15:28:35 -0500

    added nested loop to radixSort in radix

--- radix.js
+++ radix.js
@@ -18,6 +18,9 @@ const mostDigits = num => {
 const radixSort = nums => {
     let maxDigitCount = mostDigits(nums)
     for(let k = 0; k < maxDigitCount; k++){
-        
+        let digitBuckets = Array.from({length: 10}, () => [])
+        for(let i = 0; i < nums.length; i++) {
+            getDigit(nums[i], k)
+        }
     }
 }
