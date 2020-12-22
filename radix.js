4565592dfc7c5166281af5532d357cd8a4e1e0b3
github username <60378440+vmar13@users.noreply.github.com>
2020-12-22 15:22:47 -0500

    added digitCount for radix

--- radix.js
+++ radix.js
@@ -1,3 +1,8 @@
 const getDigit = (num, i) => {
     return Math.floor(Math.abs(num) / Math.pow(10, i) % 10)
 }
+
+const digitCount  = num => {
+    if(num === 0) return 1
+    return Math.floor(Math.log10(Math.abs(num)) + 1)
+}
