118df08b72c41bd094b9b7f0d5c1a9b599b17d89
github username <60378440+vmar13@users.noreply.github.com>
2020-12-22 15:24:50 -0500

    setting up mostDigits for radix

--- radix.js
+++ radix.js
@@ -6,3 +6,11 @@ const digitCount  = num => {
     if(num === 0) return 1
     return Math.floor(Math.log10(Math.abs(num)) + 1)
 }
+
+const mostDigits = num => {
+    let maxDigits = 0 
+    for(let i = 0; i < nums.length; i++) {
+
+    }
+    return maxDigits
+}
