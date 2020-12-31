d41982542cb7726dc21ca91badc33c9a8edab82a
github username <60378440+vmar13@users.noreply.github.com>
2020-12-31 11:18:31 -0500

    second performance added to newSearch

--- newSearch.js
+++ newSearch.js
@@ -4,3 +4,12 @@ for(let i = 0; i < 10000; i++) {
 }
 const end = performance.now()
 let time = end - start
+
+// second one
+performance.mark("beginLoop")
+for(let i = 0; i < 10000; i++) {
+    let ii = Math.sqrt(i)
+}
+performance.mark("endLoop")
+performance.measure("measureLoop","beginLoop","endLoop")
+let time = performance.getEntriesByName("measureLoop")[0].duration
