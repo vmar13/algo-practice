50b47a34304cc46eb16df34df055b370b2c6d288
github username <60378440+vmar13@users.noreply.github.com>
2021-02-04 17:31:44 -0500

    added quicksort

--- part.js
+++ part.js
@@ -15,3 +15,13 @@ const partition = (arr, start = 0, end = arr.length - 1) => {
     return swapIdx
 }
 
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+    if (left < right) {
+        let pivotIndex = partition(arr, left, right)
+      
+        quickSort(arr, left, pivotIndex - 1)
+      
+        quickSort(arr, pivotIndex + 1, right)
+    }
+    return arr
+}
