f67f82f65b388be62062a6a899d57d6dd8c6b61b
github username <60378440+vmar13@users.noreply.github.com>
2021-02-04 17:48:37 -0500

    started stack

---
+++ stack.js
@@ -0,0 +1,21 @@
+function Stack() {
+    this.count = 0;
+      this.storage = {};
+    
+      this.push = function (value) {
+        this.storage[this.count] = value;
+        this.count++;
+      }
+    
+      this.pop = function () {
+        if (this.count === 0) {
+          return undefined;
+        }
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+      }
+    
+     
+    }
