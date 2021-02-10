91512252bb8df520b3243694823fe3c80e588a79
github username <60378440+vmar13@users.noreply.github.com>
2021-02-10 13:54:06 -0500

    created edge case for new end time in canAttendMeetings

--- canAttendMeetings.js
+++ canAttendMeetings.js
@@ -1,9 +1,23 @@
 const canAttendMeetings = (intervals) => {
+    
+    //If you only have one meeting (a single array with 2 elements), return true
     if(intervals.length < 2) return true;
 
+    //Sort intervals arr numerically in ascending order
     intervals.sort((a,b) => {return a[0] - b[0]});
 
-    return intervals
+    //Declare a meeting end point, which grabs the first subarray and
+    //accesses that subarray's 2nd element (at index [1])
+    let end = intervals[0][1];
+
+    for(let i = 1; i < intervals.length; i++) {
+        if(end > intervals[i][0]) return false;
+        if(end < intervals[i][1]) end = intervals[i][1];
+    }
+
+    return intervals.length
 }
 
 console.log(canAttendMeetings([[7,10],[2,4]]))
+
+// console.log(canAttendMeetings([[2,4],[7,10]]))
