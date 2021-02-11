1a9dd00531578daed58299218a21b7074450f1c5
github username <60378440+vmar13@users.noreply.github.com>
2021-02-11 12:29:20 -0500

    added while loop to pasarPorTodos

--- llaves.js
+++ llaves.js
@@ -1,39 +1,41 @@
-const pasarPorTodos = cuartos => {
+const pasarPorTodos = rooms => {
     //Keep track of visited rooms (subarrays)
     //Start by adding first subarray, since we already have access
     //Llaves initially contains key for first room bc we're in it already
-    let cuartosVisitados = [];
-    cuartosVisitados.push(cuartos[0]);
-    let llaves = [0];
+    let q = []
+    q.push(rooms[0])
+    //q=[[1,3]] AoA
+    let keys = [0]
 
-    let cuarto = cuartosVisitados.shift();
-    console.log(cuarto)
+    while(q.length > 0){
+        let room = q.shift()
+        //q = []
+        //room = [1,3]
+        for (let item of room) {
+            if(!keys.includes(item)){
+                keys.push(item)
+                q.push(rooms[item])
+                //q= [[2,0,1], [0]]
+            }
+        }
+    }
+    
 }
 
 
 console.log(pasarPorTodos([[1,3],[2,0,1],[0,3], [0]]))
 
 
-// var canVisitAllRooms = function(rooms) {
-//     let n = rooms.length;
-//     let visited = Array(n).fill(0);
+// let cuartosVisitados = [];
+// cuartosVisitados.push(cuartos[0]);
+// let llaves = [0];
 
-//         function visit(i, rooms) {
-//             if(visited[i] == 1) {
-//                 return;
-//             }
-//             visited[i] =1;
-//             for(next of rooms[i]) {
-//                 visit(next, rooms);
-//             }
-//         }     
-        
-       
-//         visit(0, rooms);
-//         for(i of visited) {
-//             if(i == 0) {
-//                 return false;
-//             }
-//         }
-//         return true;  
-//     };
+// let cuarto = cuartosVisitados.shift();
+
+// for (let llave of cuarto) {
+//     if(!llaves.includes(llave)){
+//         llaves.push(llave)
+//         cuartosVisitados.push(cuartos[llave])
+//     }
+    
+// }
