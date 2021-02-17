47110266b4dfa48b6ab28790019c788a1f0d121a
github username <60378440+vmar13@users.noreply.github.com>
2021-02-17 11:13:28 -0500

    finished socks.js

--- socks.js
+++ socks.js
@@ -1,6 +1,7 @@
 function sockMerchant(n, ar) {
     let count = {};
     let total = 0; 
+    let finalTotal;
 
     for (let num of ar){
         count[num] ? count[num]+= 1 : count[num] = 1;
@@ -13,10 +14,11 @@ function sockMerchant(n, ar) {
     // return total 
 
     for (let val in count) {
-        if(count[val] % 2 !== 0) {
-            console.log(count[val])
+        if(count[val] % 2 === 0 || count[val] % 2 !== 0 && count[val] !== 1) {
+            finalTotal = Math.floor((total += count[val])/2)
         }
     }
+    return finalTotal
 }
 
 console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]))
