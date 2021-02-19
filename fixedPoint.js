5c8a1359efc7b19abb42c19c876f4c9c9329097c
github username <60378440+vmar13@users.noreply.github.com>
2021-02-19 14:17:34 -0500

    o logn alt sol fixedPoint

--- fixedPoint.js
+++ fixedPoint.js
@@ -11,3 +11,29 @@ class Solution {
         return -1
     }
 }
+
+
+//USING BINARY SEARCH TO ACHIEVE O LOG(N)
+
+class Solution {
+    solve(nums) {
+
+        let start = 0
+        let end = nums.length - 1
+        let min = -1
+        while (start <= end) {
+            let mid = Math.floor((start + end) / 2)
+            if (nums[mid] === mid) {
+                min = mid
+                end = mid - 1
+            } else if (nums[mid] < mid) {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+
+        return min
+
+    }
+}
