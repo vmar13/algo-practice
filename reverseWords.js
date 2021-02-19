cd1223949521fc458ff841d723a80acbcb8f3ed7
github username <60378440+vmar13@users.noreply.github.com>
2021-02-19 10:45:17 -0500

    refactored reverseWords.js

--- reverseWords.js
+++ reverseWords.js
@@ -1,8 +1,21 @@
-const solve = sentence => {
-    let splitSent = sentence.split(' ')
-    let reversed = splitSent.reverse()
-    let joinSent = reversed.join(' ')
-    return joinSent 
+//Given a string s of words delimited by spaces, reverse the order of words.
+
+const revSentence = sentence => {
+    reversed = sentence.split(' ').reverse().join(' ')
+    return reversed
 }
 
-console.log(solve(["hello there my friend"]))
+//Methods used:
+// .split() returns an array, specified by where you want it split (ARGUMENT)
+// .reverse() returns an array
+// .join() converts an array to a string, specified by ARGUMENT
+
+
+// const solve = sentence => {
+//     let splitSent = sentence.split(' ')
+//     let reversed = splitSent.reverse()
+//     let joinSent = reversed.join(' ')
+//     return joinSent 
+// }
+
+console.log(revSentence("hello there my friend"))
