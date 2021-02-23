8cfca9b8f55428c42d9f50e93f2e155d92250c79
github username <60378440+vmar13@users.noreply.github.com>
2021-02-23 15:39:59 -0500

    refactored bossFight

--- bossFight.js
+++ bossFight.js
@@ -1,27 +1,67 @@
-class Solution {
-    solve(fighters, bosses) {
-        let fightersTotal = 0;
-        let bossesTotal = 0;
-        let finalArr = [];
+//You are given a list of integers fighters and a two dimensional list bosses. 
+//The fighters list contains 1s and 0s with a 1 representing a fighter. 
+//Similarly, each bosses list contains 1s and 0s with a 1 representing a boss.
+//Given that fighters can beat a bosses row if it contains more fighters than bosses, 
+//return a new bosses matrix with defeated boss rows removed.
 
-        for(let num of fighters){
-            if(num === 1){
-                fightersTotal += 1
-            }
-        }
+const bossFight = (fighters, bosses) => {
+    let fightersCount = 0;
+    let bossCount = 0;
+    let winningBosses = [];
 
-        for(let i = 0; i < bosses.length; i++){
-            for(let j = 0; j < bosses[i].length; j++){
-                if(bosses[i][j] === 1){
-                    bossesTotal += 1
-                }
-            }
-            if(fightersTotal <= bossesTotal){
-                finalArr.push(bosses[i])
-            }
-            bossesTotal = 0;
-        }
-        return finalArr
-
+    for(let num of fighters){
+        num === 1 ? fightersCount+= 1 : null;
     }
+    
+    for(let subArr of bosses){
+        for(let ele of subArr){
+            ele === 1 ? bossCount+= 1 : null;
+        }
+    fightersCount <= bossCount ? winningBosses.push(subArr) : null; 
+    bossCount = 0;
+    }
+
+    return winningBosses
 }
+
+console.log(bossFight([0, 1, 1], [
+    [0, 0, 0],
+    [0, 0, 1],
+    [0, 1, 1],
+    [1, 1, 1]]))
+
+
+//SOLUTION
+//[
+//     [0, 1, 1],
+//     [1, 1, 1]
+// ]
+
+
+// class Solution {
+//     solve(fighters, bosses) {
+//         let fightersTotal = 0;
+//         let bossesTotal = 0;
+//         let finalArr = [];
+
+//         for(let num of fighters){
+//             if(num === 1){
+//                 fightersTotal += 1
+//             }
+//         }
+
+//         for(let i = 0; i < bosses.length; i++){
+//             for(let j = 0; j < bosses[i].length; j++){
+//                 if(bosses[i][j] === 1){
+//                     bossesTotal += 1
+//                 }
+//             }
+//             if(fightersTotal <= bossesTotal){
+//                 finalArr.push(bosses[i])
+//             }
+//             bossesTotal = 0;
+//         }
+//         return finalArr
+
+//     }
+// }
