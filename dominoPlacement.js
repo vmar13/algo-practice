7451b91961dc1381862f4002e7995e1402e98e6c
github username <60378440+vmar13@users.noreply.github.com>
2021-02-23 17:37:02 -0500

    refactored dominoPlacement

--- dominoPlacement.js
+++ dominoPlacement.js
@@ -1,6 +1,13 @@
-class Solution {
-    solve(n, m) {
-        let boardSize = n * m
-        return Math.floor(boardSize/2)
-    }
+//You are given integers n and m representing a board of size n by m. 
+//You also have an unlimited number of 1 by 2 dominos.
+//Return the maximum number of dominos that can be placed on the board 
+//such that they don't overlap and every domino lies entirely within the board.
+
+const dominoPlacement = (n, m) => {
+    let boardSize = n * m
+    return Math.floor(boardSize/2)
 }
+
+console.log(dominoPlacement(2, 2))
+//2
+
