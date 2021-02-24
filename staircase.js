404a24a28205cf2ffb2a29bbf947971a9ad02f0a
github username <60378440+vmar13@users.noreply.github.com>
2021-02-24 13:28:03 -0500

    added loop for each space in staircase

--- staircase.js
+++ staircase.js
@@ -10,8 +10,16 @@
 //Write a program that prints a staircase of size n.
 
 const staircase = n => {
+    //loop for the number of steps, n
     for (let i = 0; i < n; i++) {
+        //create blank string and create new string each time we loop
         let str = '';
+        // console.log(str)
+        for (let j = 0; j < n - i - 1; j++) {
+            //this loop adds one space to the string each time
+            str += ' ';
+        }
+        
         console.log(str)
     }
     
