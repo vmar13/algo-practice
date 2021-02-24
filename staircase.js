88c4b5ab929f3e39fe2fad70beeeb7c6d2073f8a
github username <60378440+vmar13@users.noreply.github.com>
2021-02-24 13:19:39 -0500

    set up staircase

---
+++ staircase.js
@@ -0,0 +1,20 @@
+//This is a staircase of size :
+
+//#
+//##
+//###
+//####
+//Its base and height are both equal to n. It is drawn using
+// # symbols and spaces. The last line is not preceded by any spaces.
+
+//Write a program that prints a staircase of size n.
+
+const staircase = n => {
+    for (let i = 0; i < n; i++) {
+        let str = '';
+        console.log(str)
+    }
+    
+}
+
+console.log(staircase(6))
