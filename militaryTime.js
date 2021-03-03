17a710fc01b06974bf09b3e70e9e9194ef8472c0
github username <60378440+vmar13@users.noreply.github.com>
2021-03-02 21:15:17 -0500

    restarting timeConversion to use string methods

--- militaryTime.js
+++ militaryTime.js
@@ -3,23 +3,15 @@
 //- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
 
 const timeConversion = s => {
-    let strArr = Array.from(s)
-    let dayOrNight = strArr.slice(-2).toString().replace(',', '')
-    let startingHour;
-    let added12;
-    let newTime = [];
-    
-    if(dayOrNight === 'PM') {
-        strArr.splice(-2)
-        startingHour =  strArr.slice(0, 2).toString().replace(',', '')
-        added12 = parseInt(startingHour) + 12
-        added12.toString()
-        // strArr.toString()
-        strArr = strArr.splice(2)
-        newTime.push(strArr)
-        // newTime = strArr.splice(0, 2, added12)
-        
-    }
-    newTime = strArr.toString().replace(startingHour, added12)
-    // console.log(newTime.unshift(added12))
+  let dayOrNight = s.charAt(8);
+  console.log(dayOrNight)
+  let hour = '';
+
+  if (dayOrNight === 'A') {
+
+  } else {
+
+  }
 }
+
+console.log(timeConversion('07:05:45PM'))
