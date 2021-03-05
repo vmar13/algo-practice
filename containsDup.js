2030c93cad42a431f3f58a7f0b03b8ca967a180d
github username <60378440+vmar13@users.noreply.github.com>
2021-03-05 13:08:10 -0500

    refactored containsDup

--- containsDup.js
+++ containsDup.js
@@ -1,15 +1,23 @@
+// var containsDuplicate = function(nums) {
+//     if (nums.length === 0) return false
+//     let count = {};
+    
+//     for (let ele of nums) {
+//         count[ele] = count[ele] + 1 || 1
+//     }
+    
+//     for (let key in count) {
+//         if(count[key] > 1) {
+//             return true
+//         }   
+//     }
+//    return false
+// };
+
 var containsDuplicate = function(nums) {
-    if (nums.length === 0) return false
-    let count = {};
-    
-    for (let ele of nums) {
-        count[ele] = count[ele] + 1 || 1
+    nums.sort((a, b) => a - b);
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) return true;
     }
-    
-    for (let key in count) {
-        if(count[key] > 1) {
-            return true
-        }   
-    }
-   return false
+    return false;
 };
