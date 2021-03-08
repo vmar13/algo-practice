e9324033f6ff28ecafe30939a8513eb89d1855fa
github username <60378440+vmar13@users.noreply.github.com>
2021-03-08 13:15:41 -0500

    added longestSubStr without repeating chars

---
+++ longestSubStr.js
@@ -0,0 +1,18 @@
+var lengthOfLongestSubstring = function(s) {
+    let k = 0;
+    let maxLength = 0;
+
+    for(i = 0; i < s.length; i++) {
+        for (j = k; j < i; j++) {
+            if (s[i] === s[j]) {
+                k = j + 1;
+                break;
+            }
+        }
+        if (i - k + 1 > maxLength) {
+            maxLength = i - k + 1;
+        }
+    }
+    return maxLength;
+}
+
--- stack.js
+++ stack.js
@@ -1,27 +1,48 @@
-function Stack() {
-    this.count = 0;
-      this.storage = {};
-    
-      this.push = function (value) {
-        this.storage[this.count] = value;
-        this.count++;
+
+const moveZeroes = nums => {
+  let counter = 0;
+
+  for(let i = 0; i < nums.length; i++){
+      if(nums[i] !== 0){
+          nums[counter] = nums[i];
+          counter += 1;
       }
+  }
+
+  for(counter; counter < nums.length; counter++){
+      nums[counter] = 0;
+  }
+
+  return nums; 
+}
+
+
+
+
+// function Stack() {
+//     this.count = 0;
+//       this.storage = {};
     
-      this.pop = function () {
-        if (this.count === 0) {
-          return undefined;
-        }
-        this.count--;
-        var result = this.storage[this.count];
-        delete this.storage[this.count];
-        return result;
-      }
+//       this.push = function (value) {
+//         this.storage[this.count] = value;
+//         this.count++;
+//       }
     
-      this.peek = function () {
-        return this.storage[this.count - 1];
-      }
+//       this.pop = function () {
+//         if (this.count === 0) {
+//           return undefined;
+//         }
+//         this.count--;
+//         var result = this.storage[this.count];
+//         delete this.storage[this.count];
+//         return result;
+//       }
     
-      this.size = function () {
-        return this.count;
-      }
-    }
+//       this.peek = function () {
+//         return this.storage[this.count - 1];
+//       }
+    
+//       this.size = function () {
+//         return this.count;
+//       }
+//     }
