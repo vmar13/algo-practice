8e1343c710fec9b42717e7a633ee11aa970ad962
github username <60378440+vmar13@users.noreply.github.com>
2021-03-08 14:02:20 -0500

    finished addDigits by using recursion

--- addDigits.js
+++ addDigits.js
@@ -1,12 +1,13 @@
 const addDigits = num => {
     let newNum = num.toString()
-    let numArr = Array.from(newNum)
     let sum = 0;
     
     for (let int of newNum) {
         sum += parseInt(int);
     }
     
-    
+    if (sum > 9) {
+        return addDigits(sum)
+    }
     return sum
 };
