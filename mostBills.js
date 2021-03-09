23a1ee2bfb6aa92bfe9f30cd36e2e20fb5bd889f
github username <60378440+vmar13@users.noreply.github.com>
2021-03-09 16:35:42 -0500

    finished mostBills

--- mostBills.js
+++ mostBills.js
@@ -12,6 +12,19 @@ const mostBills = transactions => {
     //Sort descending by trans number
     descSort.sort((a, b) => { return b[1] - a[1] })
 
-   
+    //Sort ascending alphabetically ONLY for item names with matching trans number
+    let alphaSort = descSort.sort((a, b) => {
+        if(a[1] === b[1]){
+
+            let x = a[0]
+            let y = b[0]
+
+            return x < y ? -1 : x > y ? 1 : 0
+        }
+        return a[0] - b[0]
+    })
     
+    //Convert each array element to single string and replace comma with space  
+    let finalArr = alphaSort.map(subArr => subArr.toString('').replace(',', ' '))
+    return finalArr
 }
