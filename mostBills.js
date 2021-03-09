1943dbc5cfcf7063462a4b4d315863bb06558f0a
github username <60378440+vmar13@users.noreply.github.com>
2021-03-09 16:35:14 -0500

    added descSort to mostBills

--- mostBills.js
+++ mostBills.js
@@ -6,6 +6,12 @@ const mostBills = transactions => {
         txCount[item] ? txCount[item] += 1 : txCount[item] = 1
     }
 
+    //Convert object into array 
+    let descSort = Object.entries(txCount)
+
+    //Sort descending by trans number
+    descSort.sort((a, b) => { return b[1] - a[1] })
+
    
     
 }
