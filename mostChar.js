2cef58f1771638dc5953f682a1336d4959787e6e
github username <60378440+vmar13@users.noreply.github.com>
2021-03-09 16:33:04 -0500

    setting up mostChar

---
+++ mostChar.js
@@ -0,0 +1,12 @@
+const maxChar = str => {
+    const strObj = {}
+    let maxValue = 0
+    let maxChar = ''
+
+    for(let char of str){
+        strObj[char] = strObj[char] + 1 || 1
+    }
+
+   
+    return maxChar
+}
