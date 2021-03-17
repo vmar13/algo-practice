2fee4dd162159e1d8f8c177b657c5fc843a62546
github username <60378440+vmar13@users.noreply.github.com>
2021-03-17 13:38:15 -0400

    added instructions reverseInt2

--- reverseInt2.js
+++ reverseInt2.js
@@ -1,3 +1,10 @@
+//Given a signed 32-bit integer x, return x with its digits reversed. 
+//If reversing x causes the value to go outside the signed 
+//32-bit integer range [-231, 231 - 1], then return 0.
+
+//Assume the environment does not allow you to store 64-bit integers 
+//(signed or unsigned).
+
 const reverse = x => {
     numStr = x.toString()
     let finalNum = parseInt(numStr.split('').reverse().join(''))
@@ -11,3 +18,18 @@ const reverse = x => {
 }
 
 console.log(reverse(321))
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+// Example 4:
+
+// Input: x = 0
+// Output: 0
