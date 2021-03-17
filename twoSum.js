ef2dc3cc8c12935e13208c5a1c292555a9e59ea5
github username <60378440+vmar13@users.noreply.github.com>
2021-03-17 13:42:09 -0400

    refactored and added instructions to twoSum

--- twoSum.js
+++ twoSum.js
@@ -1,3 +1,9 @@
+//Given an array of integers nums and an integer target, 
+//return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, 
+//and you may not use the same element twice.
+//You can return the answer in any order.
+
 const twoSum = (nums, target) => {    
     for (let i = 0; i < nums.length - 1; i++) {
         for (let j = i + 1; j < nums.length; j++ ) {
@@ -9,3 +15,7 @@ const twoSum = (nums, target) => {
 };
 
 console.log(twoSum([2,7,11,15], 9))
+
+//nput: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Output: Because nums[0] + nums[1] == 9, we return [0, 1].
