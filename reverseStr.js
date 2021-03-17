041c964f96bb2cad4ce2b3b1a482c5972022c335
github username <60378440+vmar13@users.noreply.github.com>
2021-03-17 13:41:06 -0400

    refactored and added instructions to reverseStr

--- reverseStr.js
+++ reverseStr.js
@@ -1,4 +1,7 @@
-var reverseString = function(s) {
+//Write a function that reverses a string. 
+//The input string is given as an array of characters s.
+
+const reverseString = s => {
     let end = s.length - 1
     
     for (let i = 0; i < end; i++) {
@@ -8,3 +11,6 @@ var reverseString = function(s) {
         end--
     }
 }
+
+//Input: s = ["h","e","l","l","o"]
+//Output: ["o","l","l","e","h"]
