38137d8d5a1f34ebbf0c575cad4531e537c2c52e
github username <60378440+vmar13@users.noreply.github.com>
2021-03-17 11:56:10 -0400

    finished twoSum

--- twoSum.js
+++ twoSum.js
@@ -1,8 +1,13 @@
 const twoSum = (nums, target) => {    
     for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = 1; j < nums.length; j++ ) {
+        for (let j = i + 1; j < nums.length; j++ ) {
             if (nums[i] + nums[j] === target) {
                 return [i, j]
+            } else if (nums[i] + nums[j] !== target) {
+                j++
+                if (nums[i] + nums[j] === target) {
+                return [i, j] 
+                }
             }
         }
     }
