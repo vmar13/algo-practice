36fbe1599c9153bc50a2751ffaf50dbed1b5105c
github username <60378440+vmar13@users.noreply.github.com>
2021-03-21 22:23:58 -0400

    added pointers to binarySearchPartition

--- binarySearchPartition.js
+++ binarySearchPartition.js
@@ -5,6 +5,13 @@ let binarySearchWithPartition = (array, element, compare = defaultCompare) => {
 	const middle = Math.floor(array.length / 2);
 	const comparison = compare(element, array.get(middle));
 
-	
-};
+	if (comparison === 0) {
+		return middle;
+	}
 
+	const [left, right] =
+		comparison === -1 ? [0, middle - 1] : [middle + 1, array.length];
+
+
+	return subIndex === -1 ? -1 : left + subIndex;
+};
