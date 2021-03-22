dafcd472364c07587b5c592679b1ae54b6c0c9a6
github username <60378440+vmar13@users.noreply.github.com>
2021-03-22 12:12:54 -0400

    setting up firstUniqChar

---
+++ firstUniqChar.js
@@ -0,0 +1,11 @@
+//Given a string, find the first non-repeating character in it and 
+//return its index. If it doesn't exist, return -1.
+
+const firstUniqChar = s => {
+    s = s.split('')
+    let charObj = {}; 
+    let firstNonRepeat = [];
+    
+   
+    
+};
