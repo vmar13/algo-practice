dd8b4d102c91fb079338a72c78c5630fdd2772c9
github username <60378440+vmar13@users.noreply.github.com>
2021-03-24 12:30:40 -0400

    setting up romanToInt

---
+++ romanToInt.js
@@ -0,0 +1,21 @@
+var romanToInt = function(s) {
+    let roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    
+    let sum = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (roman[s[i]] < roman[s[i + 1]]) {
+            sum += roman[s[i + 1]] - roman[s[i]]
+            i++
+        } 
+    }
+    return sum
+};
