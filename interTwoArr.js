c963ae016b064eb8668d4f61cbda65068cd77b75
github username <60378440+vmar13@users.noreply.github.com>
2021-03-24 16:45:07 -0400

    added ex inputs to interTwoArr

--- interTwoArr.js
+++ interTwoArr.js
@@ -16,3 +16,10 @@ const intersection = (nums1, nums2) => {
     
     return Array.from(uniq);
 }
+
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2]
+
+//Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//Output: [9,4]
+//Explanation: [4,9] is also accepted.
