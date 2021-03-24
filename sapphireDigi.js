9fe5bf84efad6827ddf79085039b82a6e8f3cf91
github username <60378440+vmar13@users.noreply.github.com>
2021-03-23 22:10:40 -0400

    added fetch in getDeployedDates

--- sapphireDigi.js
+++ sapphireDigi.js
@@ -21,3 +21,21 @@ document.addEventListener('DOMContentLoaded', e => {
 getMostRecentDeploys()
 
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const getDeployedDates = () => {
+        fetch('https://gist.githubusercontent.com/gretchenziegler/853c4f709d45176aa44c8e5aee864cac/raw/010a4c44455ffc93b8039935cfc4e0dff41ae502/hcsc.json')
+        .then(res => res.json())
+        // .then(clientEnvirons => console.log(clientEnvirons))
+        // .then(data => console.log(data.client_environments))
+        .then(data => {
+            let clientEnvirons = data.client_environments //AoO
+            let mostRecDeploys = clientEnvirons.map(objEle => objEle.most_recent_deploys)
+            let platAPI = mostRecDeploys.map(depEle => depEle['platform-api'])
+            let deployedDates = platAPI.map(dateObj => dateObj.deployed_at)
+            deployedDates.forEach(date => console.log(date))
+        })
+    }
+
+    getDeployedDates()
+})
