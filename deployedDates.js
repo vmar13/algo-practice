a2ffcbc714544dc610edb155a4d041caa7af30a7
github username <60378440+vmar13@users.noreply.github.com>
2021-03-23 22:12:23 -0400

    filtered down to get array of objs in getDeployedDates

--- deployedDates.js
+++ deployedDates.js
@@ -2,8 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const getDeployedDates = () => {
         fetch('https://gist.githubusercontent.com/gretchenziegler/853c4f709d45176aa44c8e5aee864cac/raw/010a4c44455ffc93b8039935cfc4e0dff41ae502/hcsc.json')
         .then(res => res.json())
-        .then(clientEnvirons => console.log(clientEnvirons))
-       
+        // .then(clientEnvirons => console.log(clientEnvirons))
+        // .then(data => console.log(data.client_environments))
+        .then(data => {
+            let clientEnvirons = data.client_environments //AoO
+            
+        })
     }
 
     getDeployedDates()
