e84d7ca3dbdbf03a9c52c0748213a98b26165139
github username <60378440+vmar13@users.noreply.github.com>
2021-03-30 16:02:38 -0400

    added 2dArr

---
+++ 2dArr.js
@@ -0,0 +1,23 @@
+const hourGlass = array => {
+    let sumArr = [];
+    let i = 0;
+    let j = 0;
+    let x = 0;
+
+    while (x = 16) {
+        if (x === 4) {
+            i++;
+            j = 0;
+        }
+    }
+}
+
+let arr = [
+        [1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0],
+        [0, 0, 2, 4, 4, 0],
+        [0, 0, 0, 2, 0, 0],
+        [0, 0, 1, 2, 4, 0]
+]
+console.log(hourGlass(arr))
