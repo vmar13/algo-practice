2811eeae8a791f3494d48dcf69fda3c77d554459
github username <60378440+vmar13@users.noreply.github.com>
2021-03-31 16:29:13 -0400

    added noisyPalindrome

---
+++ noisyPalindrome.js
@@ -0,0 +1,13 @@
+const noisyPalindrome = s => {
+    s = s.replace(/[0-9]/g, '')
+    s = s.replace(/[^a-z]/g, '')
+
+    let revStr = s.split('').reverse().join('')
+   if (s === revStr) {
+       return true
+   } else {
+       return false
+   }
+}
+
+console.log(noisyPalindrome("a92bcbXa"))
