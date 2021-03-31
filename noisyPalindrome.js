0c063497ba4758bb1efa9f6d8a4789526c3070c3
github username <60378440+vmar13@users.noreply.github.com>
2021-03-31 16:30:33 -0400

    refactored noisyPalindrome

--- noisyPalindrome.js
+++ noisyPalindrome.js
@@ -1,6 +1,7 @@
 const noisyPalindrome = s => {
-    s = s.replace(/[0-9]/g, '')
-    s = s.replace(/[^a-z]/g, '')
+    // s = s.replace(/[0-9]/g, '')
+    // s = s.replace(/[^a-z]/g, '')
+    s = s.replace(/[^a-z]+/g, "")
 
     let revStr = s.split('').reverse().join('')
    if (s === revStr) {
