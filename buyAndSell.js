dc74bc485a3ee7ef48fbb4e530f5f8e95c3ad44e
github username <60378440+vmar13@users.noreply.github.com>
2021-04-09 11:45:24 -0400

    refactored buyAndSell

--- buyAndSell.js
+++ buyAndSell.js
@@ -1,15 +1,50 @@
-let prices = [10,6,3,5,7,4]
+//You are given an array prices where prices[i] is the price of a given stock on the ith day.
+//Find the maximum profit you can achieve. You may complete as many transactions as you like 
+//(i.e., buy one and sell one share of the stock multiple times).
+//Note: You may not engage in multiple transactions simultaneously 
+//(i.e., you must sell the stock before you buy again).
 
-const maxProfit = prices => {
-    let profit = 0; 
-    
-    for (let i = 0; i < prices.length - 1; i++) {
-        if(prices[i] < prices[i + 1]) {
-            profit += prices[i + 1] - prices[i]
+//Input: prices = [7,1,5,3,6,4]
+//Output: 7
+//Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+//Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+let prices = [7,1,5,3,6,4]
+// 7, 1
+// 1, 5
+// 5, 3
+// 3, 6
+// 6, 4
+
+const maxProfit = nums => {
+    let profit = 0;
+    //sell day should be higher than buy day; buy low/sell high
+    //i is buy AND j is sell
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = 1; j < nums.length; j++) {
+            console.log(nums[i],nums[j])
+            if (nums[i] < nums[j]) {
+                profit += nums[j] - nums[i]
+                i++
+            } else {
+                i++
+            }
         }
     }
     return profit
 }
+
+
+// const maxProfit = prices => {
+//     let profit = 0; 
+    
+//     for (let i = 0; i < prices.length - 1; i++) {
+//         if(prices[i] < prices[i + 1]) {
+//             profit += prices[i + 1] - prices[i]
+//         }
+//     }
+//     return profit
+// }
 
 console.log(maxProfit(prices))
 
