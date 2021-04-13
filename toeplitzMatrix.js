d1718b3c26e93c89c684339fe64222ca9b0aab21
github username <60378440+vmar13@users.noreply.github.com>
2021-04-13 13:52:14 -0400

    refactored toeplitzMatrix

--- toeplitzMatrix.js
+++ toeplitzMatrix.js
@@ -5,12 +5,9 @@
 const toeplitz = matrix => {
     for (let i = 1; i < matrix.length; i++) {
         for (let j = 1; j < matrix[i].length; j++) {
-            if (matrix[i][j] !== matrix[i - 1][j - 1]) {
-                return false
-            } 
+            if (matrix[i][j] !== matrix[i - 1][j - 1]) return false
         }
     }
-
     return true
 }
 
