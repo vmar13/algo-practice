b10e2dc4880aa2caa41a19bf5fb27b15bc0fbaba
github username <60378440+vmar13@users.noreply.github.com>
2021-04-14 12:55:26 -0400

    refactored and added more notes to longestCommPrefix

--- longestCommPrefix.js
+++ longestCommPrefix.js
@@ -7,32 +7,32 @@ const longestCommPref = arrStrs => {
     let compare = arrStrs[0]; //word to compare every other wordEle to; first wordEle in array
 
     for (let i = 0; i < compare.length; i++) {  //6
-        for (let j = 1; j < arrStrs.length; j++) {
+        // console.log(`i loop: ${i}`)
+        for (let j = 1; j < arrStrs.length; j++) { //3
+            // console.log(`jloop ${j}: ${arrStrs[j][i]}, ${arrStrs[j]}`)
             if (arrStrs[j][i] !== compare[i]) {    // if 'f' in 'flow' !== 'f' in 'flower'
-            // console.log(arrStrs[i][j], arrStrs[i + 1][j])
                 return prefix
             }
         }
         prefix += compare[i]   // if 'f' in flow === 'f' in 'flower', add it to prefix
+        // console.log(prefix)
     }
     return prefix
 }
 
-// const longestCommPref = strs => {
-//     if(strs.length === 0) return ""
-//     let prefix = ""
-//     let compare = strs[0]
-
-//     for(let i = 0; i < compare.length; i++){
-//        for(let j = 1; j < strs.length; j++){
-//            if(strs[j][i] !== compare[i]){
-//               return prefix
-//            }
-//        }
-//         prefix += compare[i]
-//     }
-//     return prefix
-// };
+//***** WHAT'S HAPPENING FOR EVERY LOOP *****/
+// i loop: 0
+// j loop 1: f, flow
+// j loop 2: f, flight
+// f
+// i loop: 1
+// j loop 1: l, flow
+// j loop 2: l, flight
+// fl
+// i loop: 2
+// j loop 1: o, flow
+// j loop 2: i, flight
+// fl
 
 console.log(longestCommPref(["flower","flow","flight"]))
 
