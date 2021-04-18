69f0e98f3285cb6f96edf64a10503c53f0cb237f
github username <60378440+vmar13@users.noreply.github.com>
2021-04-18 12:43:19 -0400

    added iterative solution to factorial

---
+++ factorial.js
@@ -0,0 +1,11 @@
+//*** BASIC ITERATIVE SOLUTION ***/
+const factorial = num => {
+    let total = 1;
+
+    for (let i = num; i > 1; i--) {
+        total *= i
+    }
+    return total;
+}
+
+console.log(factorial(4))
