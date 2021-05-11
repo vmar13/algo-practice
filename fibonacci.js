6f6fe166566219efdc4a1da0988e8f882acf484c
github username <60378440+vmar13@users.noreply.github.com>
2021-05-10 22:17:27 -0400

    added fib sol using memoization

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,4 @@
-fibonacci = num => {
+const fibonacci = num => {
     let a = 1, b = 0, temp;
   
     while (num >= 0){
@@ -13,8 +13,25 @@ fibonacci = num => {
 
 //recursive solution
 
-const fibonacci = num => {
-    if (num <= 1) return 1;
-  
-    return fibonacci(num - 1) + fibonacci(num - 2);
+const fib1 = num => {
+  if (num <= 2) return 1;
+  return fib(num - 1) + fib(num - 2);
+}
+
+console.log(fib1(4))
+// 1, 1, 2, 3
+
+//solution with memoization
+function fib(n, prevValues = []) {
+  if (prevValues[n] != null) return prevValues[n];
+  let result;
+  if (n <= 2) {
+    result = 1;
+  } else {
+    result = fib(n - 1, prevValues) + fib(n - 2, prevValues);
   }
+  prevValues[n] = result;
+  return result;
+}
+
+console.log(fib(40))
