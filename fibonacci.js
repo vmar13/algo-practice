91ee4d7b40fc7ab307044a636b922bdc5947a3e7
github username <60378440+vmar13@users.noreply.github.com>
2021-05-11 13:13:57 -0400

    added new fib sol

--- fibonacci.js
+++ fibonacci.js
@@ -1,37 +1,57 @@
-const fibonacci = num => {
-    let a = 1, b = 0, temp;
-  
-    while (num >= 0){
-      temp = a;
-      a = a + b;
-      b = temp;
-      num--;
-    }
-  
-    return b;
-  }
+//Fibonacci sequence: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
 
-//recursive solution
+const fib = n => {
+  if (n < 3) return 1;
+
+  let prev = 1;
+  let curr = 1;
+
+  for (let i = 2; i < n; i++) {
+    const next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+  return curr;
+}
+
+console.log(fib(3))
+
+// //recursive solution
 
 const fib1 = num => {
   if (num <= 2) return 1;
-  return fib(num - 1) + fib(num - 2);
+  return fib1(num - 1) + fib1(num - 2);
 }
 
-console.log(fib1(4))
-// 1, 1, 2, 3
+//console.log(fib1(5)) //Find the 5th num in the fibonacci sequence
+// //fib1(5 - 1) + fib1(5 - 2) // 4 + 3
+// //fib1(4 - 1) + fib1(4 - 2) // 3 + 2
+// //fib1(3 - 1) + 1          // 2 + 1 = 3
+// //1 + 1 = 2, so 3 + 2 = 5
 
-//solution with memoization
-function fib(n, prevValues = []) {
-  if (prevValues[n] != null) return prevValues[n];
-  let result;
-  if (n <= 2) {
-    result = 1;
-  } else {
-    result = fib(n - 1, prevValues) + fib(n - 2, prevValues);
-  }
-  prevValues[n] = result;
-  return result;
-}
+//fib1(2) // 1
+// //returns 1
 
-console.log(fib(40))
+//fib1(3) // 2
+// fib1(3 - 1) + fib1(3 - 2)  // 2 + 1
+// 1 + 1 = 2
+
+//fib1(4) // 3
+// fib1(4 - 1) + fib1(4 - 2) // 3 + 2
+// fib1(3 - 1) + 1           // 2 + 1 = 3
+
+
+// //solution with memoization
+// function fib(n, prevValues = []) {
+//   if (prevValues[n] != null) return prevValues[n];
+//   let result;
+//   if (n <= 2) {
+//     result = 1;
+//   } else {
+//     result = fib(n - 1, prevValues) + fib(n - 2, prevValues);
+//   }
+//   prevValues[n] = result;
+//   return result;
+// }
+
+// console.log(fib(40))
