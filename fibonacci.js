90aa695ecc535aba3cea0f11fc92aacf1938b0a9
github username <60378440+vmar13@users.noreply.github.com>
2021-05-11 21:50:41 -0400

    added memoization example

--- fibonacci.js
+++ fibonacci.js
@@ -33,17 +33,3 @@ const fib = num => {
 //------------------------------------------------------------------------------------------------------------------//
 
 
-// //solution with memoization
-// function fib(n, prevValues = []) {
-//   if (prevValues[n] != null) return prevValues[n];
-//   let result;
-//   if (n <= 2) {
-//     result = 1;
-//   } else {
-//     result = fib(n - 1, prevValues) + fib(n - 2, prevValues);
-//   }
-//   prevValues[n] = result;
-//   return result;
-// }
-
-// console.log(fib(40))
