c7d0be425883facd2f2e0184933bcda2e125f82b
github username <60378440+vmar13@users.noreply.github.com>
2021-05-12 17:50:13 -0400

    added 2 recursive solutions for sumRange

--- recursionBasics.js
+++ recursionBasics.js
@@ -1,6 +1,8 @@
+//********PROBLEM #1 ******************//
 //Write a non-recursive function that counts down from a given number n all the 
 //way down to 0. 
 
+//-------NON-RECURSIVE SOLUTION-------//
 const coundown = n => {
     for (let i = n; i >= 0; i--) {
         console.log(i);
@@ -9,10 +11,53 @@ const coundown = n => {
 
 // console.log(coundown(10))
 
+//-------RECURSIVE SOLUTION--------//
 const countdownRec = n => {
-    if (n === 0) return n;
+    if (n <= 0) return n;
     console.log(n)
     return countdownRec(n - 1);
 }
 
-console.log(countdownRec(10))
+// console.log(countdownRec(10))
+
+
+
+//********PROBLEM #2 ******************//
+
+//Write a non-recursive function that sums all numbers from itself down to 1
+
+//-------NON-RECURSIVE SOLUTION-------//
+const sumRange = n => {
+    let count = 0;
+    for (let i = n; i > 0; i--) {
+        count += i;
+    }
+    return count;
+}
+
+// console.log(sumRange(3))
+
+//-------RECURSIVE SOLUTION #1--------//
+const sumRangeRec = n => {
+    if (n <= 0) return n;
+    return n + sumRangeRec(n - 1)
+}
+
+// console.log(sumRangeRec(3))
+
+// 3 + sumRangeRec(2)
+//      2 + sumRangeRec(1)
+//          1
+
+//-------RECURSIVE SOLUTION #2--------//
+const sumRangeRecursion = (n, total = 0) => {
+    if (n <= 0) return total;
+    return sumRangeRecursion(n - 1, total + n);
+}
+
+//sumRangeRec(2, 3)
+        //sumRangeRec(1, 5)
+                //sumRangeRec(0, 6)
+                    //return 6
+
+console.log(sumRangeRecursion(3))
