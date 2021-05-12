8096eb1378f0ff622441dc16c3170444f50c3d9a
github username <60378440+vmar13@users.noreply.github.com>
2021-05-11 22:00:39 -0400

    added memoization with recursion

--- memoization.js
+++ memoization.js
@@ -13,7 +13,24 @@ function square(n) {
     return result;
 }
 
-console.log(square(30000))
-// console.log(prevValues)
-console.log(square(30000))
-console.log(square(30000))
+// console.log(square(30000))
+//console.log(prevValues)
+// console.log(square(30000))
+// console.log(square(30000))
+
+//-----------USING MEMOIZATION WITH RECURSION---------//
+const fib = (n, prevValues = []) => {
+    if (prevValues[n] != null) {
+        return prevValues[n];
+    }
+    let result;
+    if (n <= 2) {
+        result = 1;
+    } else {
+        result = fib(n - 1, prevValues) + fib(n - 2, prevValues)
+    }
+    prevValues[n] = result;
+    return result;
+}
+
+console.log(fib(5))
