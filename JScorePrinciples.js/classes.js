0f9050c1b5d559ce7ee095b1605816e0264ad254
github username <60378440+vmar13@users.noreply.github.com>
2021-05-13 09:57:19 -0400

    created more instance methods for student class

--- JScorePrinciples.js/classes.js
+++ JScorePrinciples.js/classes.js
@@ -2,12 +2,38 @@ class Student {
     constructor(firstName, lastName) {      //basic class constructor
         this.firstName = firstName;         //"this" will refer to THAT newly created obj
         this.lastName = lastName;
+        this.late = 0;
+        this.scores = [];
     }
 
-    fullName = () => {                     //basic instance method
-        console.log(`My full name is ${this.firstName} ${this.lastName}.`)
+    fullName() {                     //basic instance method
+        return `My full name is ${this.firstName} ${this.lastName}.`
+    }
+
+    markLate() {
+        this.late += 1;
+        if (this.late >= 3) {
+            return 'YOU ARE EXPELLED!'
+        }
+        return `${this.firstName} has been late ${this.late} time(s).`
+    }
+
+    addScore(score) {
+        this.scores.push(score);
+        return this.scores;
+    }
+
+    calculateAvg() {
+        let sum = this.scores.reduce(function(a, b){return a + b});
+        return sum/this.scores.length;
     }
 }
 
 let vani = new Student('Vanessa', 'Martinez')
 vani.fullName()
+// console.log(vani.markLate())
+// console.log(vani.markLate())
+// console.log(vani.markLate())
+console.log(vani.addScore(7))
+console.log(vani.addScore(90))
+console.log(vani.calculateAvg())
