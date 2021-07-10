523c18e810b4b8ac83fdd36d37484268a2031516
github username <60378440+vmar13@users.noreply.github.com>
2021-07-10 14:41:05 -0400

    refactored addDigits

--- addDigits.js
+++ addDigits.js
@@ -3,15 +3,37 @@
 // has only one digit.
 
 const addDigits = num => {
-    let newNum = num.toString()
+    let numLength = num.toString().match(/\d/g).length; // Convert into a string, then use regex. \d looks for all digits and g is a global search. Then call .length to get length of digits on that matching search.
+    if (numLength === 1) return num;
     let sum = 0;
+    let splitNum;
+
+    if (numLength > 1) {
+        splitNum = num.toString().split('').map(ele => parseInt(ele)) // Convert num to string, convert into array of digits, convert each string digit back into an integer // [3, 8]
+        for (let digit of splitNum) {
+            sum += digit
+        }
+    } 
     
-    for (let int of newNum) {
-        sum += parseInt(int);
-    }
+    return addDigits(sum);
+}
+
+console.log(addDigits(99))
+
+
+
+
+// const addDigits = num => {
+//     let newNum = num.toString()
+//     let sum = 0;
     
-    if (sum > 9) {
-        return addDigits(sum)
-    }
-    return sum
-};
+//     for (let int of newNum) {
+//         sum += parseInt(int);
+//     }
+    
+//     if (sum > 9) {
+//         return addDigits(sum)
+//     }
+//     return sum
+// };
+
