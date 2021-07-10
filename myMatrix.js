4fe752a5dfdd7dde02065228fee1c4ad8cdf72d4
github username <60378440+vmar13@users.noreply.github.com>
2021-07-10 12:28:42 -0400

    reviewing basics of spread operator

--- myMatrix.js
+++ myMatrix.js
@@ -3,29 +3,47 @@ let myMatrix = [[1, 2, 3, 4],
                 [11,16,15,6],
                 [10,9, 8, 7]];
 
-function unroll(matrix) {
-
-//exit condition
-if(myMatrix.length === 0) return;
-
-
-//top
-console.log(...myMatrix.shift())
-
-//right side (last elem of each)
-console.log(...myMatrix.map(arrayEle => arrayEle.pop()))
-
-//bottom in reverse (.reverse())
-console.log(...myMatrix.pop().reverse())
-
-//left side (first elem of each)
-console.log(...myMatrix.map(arrayEle => arrayEle.shift()).reverse())
-
-
-unroll(matrix)
-
+const unroll = matrix => {
+    console.log(...matrix.shift())
 }
-unroll(myMatrix);
+
+console.log(unroll(myMatrix))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function unroll(matrix) {
+
+// //exit condition
+// if(myMatrix.length === 0) return;
+
+// //top
+// console.log(...myMatrix.shift())
+
+// //right side (last elem of each)
+// console.log(...myMatrix.map(arrayEle => arrayEle.pop()))
+
+// //bottom in reverse (.reverse())
+// console.log(...myMatrix.pop().reverse())
+
+// //left side (first elem of each)
+// console.log(...myMatrix.map(arrayEle => arrayEle.shift()).reverse())
+
+
+// unroll(matrix)
+
+// }
+// unroll(myMatrix);
 
 
 // end result: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
---
+++ spreadOperator.js
@@ -0,0 +1,12 @@
+//Revisiting the Basics - Common uses for spread operator 
+
+let friends = ['Danetsy', 'Ben', 'Tony', 'Paquito']
+let newFriends = [...friends] //making a copy of an array
+let bothFriends = [...friends, ...newFriends] //concatenating two arrays without mutating originals
+// console.log(bothFriends)
+
+let nums = [23, 46, 65, 89]
+// console.log(Math.max(...nums)) //passing arguments to Math obj methods
+
+console.log(...nums) // console logging an array like a string
+// 23 46 65 89
